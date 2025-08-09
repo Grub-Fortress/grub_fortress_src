4d2be39388dbad51d007bc6b692a6f263f0d5f92
GrubPL <grubasekplpl@gmail.com>
2025-08-09 09:50:12 +0200

    misc commit

--- src/game/shared/tf/tf_item_inventory.cpp
+++ src/game/shared/tf/tf_item_inventory.cpp
@@ -50,7 +50,7 @@ using namespace GCSDK;
 
 #define LOCAL_LOADOUT_FILE		"cfg/local_loadout.txt"
 
-ConVar bf_cosmetic_restrictions("bf_cosmetic_restrictions", "1", FCVAR_REPLICATED, "Disable holiday restrictions on items.");
+ConVar bf_cosmetic_restrictions("bf_cosmetic_restrictions", "0", FCVAR_REPLICATED || FCVAR_DEVELOPMENTONLY, "Disable holiday restrictions on items.");
 
 #ifdef CLIENT_DLL
 //-----------------------------------------------------------------------------
