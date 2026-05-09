9f670794f42864fecc429d0553c55a19d5da5d49
GrubPL <grubasekplpl@gmail.com>
2026-05-09 16:24:31 +0200

    Restore "Fix TF_COND_ROCKETPACK not being removed if player has no RocketPack"

--- src/game/shared/tf/tf_player_shared.cpp
+++ src/game/shared/tf/tf_player_shared.cpp
@@ -3204,6 +3204,11 @@ void CTFPlayerShared::ConditionThink( void )
 #endif
 				}
 			}
+			else
+			{
+				// RocketPack not found, may have been unequipped, or we may be a Medic who inherited the cond
+				RemoveCond( TF_COND_ROCKETPACK );
+			}
 		}
 	}
 
